b28aad9126577ea71a30491fbd814fde15644e03
inx-jaydeepgadhavi <jaydeepg.inexture@gmail.com>
2024-09-06 19:39:33 +0530

    Changed design in Affix component and deleted the unwanted code.

--- client/src/common/Affix.jsx
+++ client/src/common/Affix.jsx
@@ -1,7 +1,7 @@
 import { ArrowUpward } from "@mui/icons-material";
 import { useWindowScroll } from "@mantine/hooks";
 import { Affix, Transition, rem } from "@mantine/core";
-import { Button } from "@mui/material";
+import { Paper } from "@mui/material";
 const Scrollup = () => {
   const [scroll, scrollTo] = useWindowScroll();
 
@@ -10,25 +10,25 @@ const Scrollup = () => {
       <Affix position={{ bottom: 20, right: 20 }}>
         <Transition transition="slide-up" mounted={scroll.y > 0}>
           {(transitionStyles) => (
-            <Button
+            <Paper
               sx={{
                 ...transitionStyles,
                 backgroundColor: "#0F172A",
                 color: "#fff",
-                borderRadius: rem(8),
-                // padding: rem(1),
+                borderRadius: "50%",
+                padding: rem(6),
                 boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
                 "&:hover": {
-                  backgroundColor: "#0a1224", // Darker shade on hover
+                  backgroundColor: "#0a1224",
                 },
                 ":focus": {
-                  outline: `2px solid ${rem(2)} #0056b3`, // Focus outline
+                  outline: `2px solid ${rem(2)} #0056b3`,
                 },
               }}
               onClick={() => scrollTo({ y: 0 })}
             >
-              <ArrowUpward />
-            </Button>
+              <ArrowUpward sx={{ width: "2rem", height: "2rem" }} />
+            </Paper>
           )}
         </Transition>
       </Affix>
